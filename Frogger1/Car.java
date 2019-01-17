43b7fae11b0e1322db2a0d9c6c53495f961d2ecf
Chase09 <43047037+Chase09@users.noreply.github.com>
2019-01-17 15:12:40 -0600

    Add files via upload

---
+++ Frogger1/Car.java
@@ -0,0 +1,48 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Car here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Car extends Actor
+{
+    /**
+     * Act - do whatever the Car wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        //getImage().scale(60,60);
+        move();
+        checkForFrogs();
+        atEdge();
+        
+        
+    }    
+   
+    private void move()
+    {
+        move(1);
+    }
+    
+    private void atEdge()
+    {
+        if(isAtEdge() == true)
+        {
+            getWorld().removeObject(this);
+        }
+    }
+    
+    private void checkForFrogs()
+    {
+        if(isTouching(Frog.class)==true)
+        {
+             removeTouching(Frog.class);
+             GameOver gameover = new GameOver();
+             getWorld().addObject(gameover, getWorld().getWidth()/2, getWorld().getHeight()/2 -70);
+             Greenfoot.stop();
+        }
+    }
+}
